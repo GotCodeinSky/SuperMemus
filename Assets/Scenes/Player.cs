94ad901cbc3378509a07f067864bdd6fe4f40d1e
Анастасия Кулагина <anastasiakulagina@MacBook-Pro-Anastasia.local>
2021-12-09 13:30:37 +0300

    Init. Jump

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // PlayerMove - PascalCase
+    // playerMove - camelCase
+    // player_move - snake-case/underscore_case
+    // player-move - kebab-case
+
+    //int 5
+    //float 5.6f
+    //string "Privet"
+    //boolean true false
+
+    [Header("Параметры Кирилла")]
+    [SerializeField] protected int PlayerSpeed = 5;
+    [SerializeField] protected float PlayerJump = 2565.5f;
+    private float moveX;
+
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+       PlayerMove();
+    }
+
+   void PlayerMove()
+   {
+       moveX = Input.GetAxis("Horizontal");
+       if (Input.GetButtonDown("Jump")) 
+       {
+            Jump();
+       } 
+   } 
+
+    void Jump()
+    {
+       GetComponent<Rigidbody2D>().AddForce(Vector2.up * PlayerJump);
+    }
+   
+} 
